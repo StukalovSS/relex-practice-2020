82992fb492bcce21b80c0b38f84b7c223e4566b3
LomovaAA <anastlomova14@gmail.com>
2020-10-06 09:48:48 +0300

    edit server

--- lomova/server/server.js
+++ lomova/server/server.js
@@ -3,21 +3,56 @@ const express = require('express');
 const app = express();
 
 
+function random(min, max) {
+    return Math.round(min - 0.5 + Math.random() * (max - min + 1));
+}
+
 class Circle {
-        constructor(x, y, r) {
-            this.x = x;
-            this.y = y;
-            this.r = r;
-        }
+    x;
+    y;
+    r;
+    constructor(x, y, r) {
+        this.x = x;
+        this.y = y;
+        this.r = r; 
     }
+}
+
+class Vector {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Player {
+    constructor(x, y, r, id) {
+        this.x = x;
+        this.y = y;
+        this.r = r; 
+        this.id = id; 
+    }
+    update(a, b) {
+        const vect = new Vector(a, b);
+        vect.length = 3;
+        this.x = this.x +  (vect.x - this.x) * 0.1;
+        this.y = this.y +  (vect.y - this.y) * 0.1; 
+    }
+}
+
+
+
+
 
 let players = [];
 let food = [];
-let min = Math.ceil(-300);
+let min = Math.ceil(-700);
 let max = Math.floor(700);
-let player1 = new Circle(0, 0, 36);
-players.push(player1);
 
+let minP = Math.ceil(-300);
+let maxP = Math.floor(300);
+
+let currId = 0;
 for (let i = 0; i < 100; i++) {
     let obj = new Circle(Math.floor(Math.random() * (max - min)) + min, Math.floor(Math.random() * (max - min)) + min, 20);
     food.push(obj);
@@ -31,17 +66,24 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.get('/', (req, res) => {
-    // текущие координаты курсора
-    // let x = +req.query.x;
-    // let y = +req.query.y;
-    
-    res.send(JSON.stringify({"f": food, "p": player1}));
-    console.log(req.query.x, req.query.y)
+app.get('/new_player', (req, res) => {
+    let newId = currId++;
+    let newPlayer = new Player(Math.floor(Math.random() * (maxP - minP)) + minP, Math.floor(Math.random() * (maxP - minP)) + minP, 36, newId);
+    players.push(newPlayer);
+    res.send(JSON.stringify({"id": newId}));
+})
 
-    // res.writeHead(200, {
-    //     'Content-Type': 'text/plain; charset = utf-8'
-    // });
+app.get('/state', (req, res) => {
+    let player_id = +req.query.player_id;
+    let playerCurr;
+    players.forEach(element => {
+        if(element.id == player_id){
+            playerCurr = element;
+        }
+    });
+    playerCurr.update(+req.query.x, +req.query.y);
+    let resPlayers = players.filter(el => el != playerCurr);
+    res.send(JSON.stringify({"f": food, "allP": resPlayers, "pCurr": playerCurr}));
 });
 
 app.listen(3000, () => {
