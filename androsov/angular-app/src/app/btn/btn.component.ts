cdd9db06e7fd2d0b266d578feed234e9e9e67f04
mxzpytlk <mxzpytlk@gmail.com>
2020-10-09 17:27:50 +0300

    Add component-btn

---
+++ androsov/angular-app/src/app/btn/btn.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-btn',
+  templateUrl: './btn.component.html',
+  styleUrls: ['./btn.component.css']
+})
+export class BtnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addClick() {
+    document.getElementById( 'hello' ).style.color = 'green';
+  }
+
+}
