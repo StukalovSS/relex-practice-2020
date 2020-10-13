6867341e8d817d13067627fbef438a30ab9079fd
mxzpytlk <mxzpytlk@gmail.com>
2020-10-13 19:57:21 +0300

    Users can add and delete images and add ViewGuard to confirm on full screen increasing

--- androsov/my-gallery-ang/src/app/app.routing.ts
+++ androsov/my-gallery-ang/src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FullScreenComponent } from './full-screen/full-screen.component';
+import { ViewGuard } from './view.guard';
 
 const routes: Routes = [
     {
@@ -9,7 +10,8 @@ const routes: Routes = [
         component: HomeComponent
     }, {
         path: 'view/:src',
-        component: FullScreenComponent
+        component: FullScreenComponent,
+        canActivate: [ViewGuard]
     }
 ]
 
