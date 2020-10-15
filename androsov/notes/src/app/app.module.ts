898af28343a561254345fa7c10abea41304a77b3
mxzpytlk <mxzpytlk@gmail.com>
2020-10-15 20:11:08 +0300

    Add note component style

---
+++ androsov/notes/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContainerComponent } from './container/container.component';
+import { SectionComponent } from './section/section.component';
+import { NoteComponent } from './note/note.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContainerComponent,
+    SectionComponent,
+    NoteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
