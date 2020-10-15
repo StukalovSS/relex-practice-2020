09260b28c3fdb211b7209ced2d7a7f7f85de97dc
Roman Baklanov <wackier.leaf@yandex.ru>
2020-10-15 21:09:21 +0300

    Добавлено приложение PushPin(заметки)

---
+++ Baklanov/AngularApps/PushPin/src/app/section/section.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+import { faCogs } from '@fortawesome/free-solid-svg-icons';
+@Component({
+  selector: 'app-section',
+  templateUrl: './section.component.html',
+  styleUrls: ['./section.component.css']
+})
+export class SectionComponent implements OnInit {
+  faEllipsisV = faEllipsisV;
+  faCogs = faCogs;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
