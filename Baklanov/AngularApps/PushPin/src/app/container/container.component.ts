0370f59f10494cb20dd4e73e18518b7a7f2332de
Roman Baklanov <wackier.leaf@yandex.ru>
2020-10-16 16:11:57 +0300

    sync

--- Baklanov/AngularApps/PushPin/src/app/container/container.component.ts
+++ Baklanov/AngularApps/PushPin/src/app/container/container.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import {ISection} from "./section.interface";
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.css']
 })
-export class ContainerComponent implements OnInit {
+export class ContainerComponent implements OnInit, ISection {
+  sectionTitle:string;
+  arrayOfnotes:any[];
+  clickOnOptionButton:any;
+  clickOnAddButton:any;
+  arrayOfSections = [];
   faPlus = faPlus;
   constructor() { }
 
