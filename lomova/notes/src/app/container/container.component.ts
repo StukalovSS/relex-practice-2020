81fbd2b97ee1db9ff5f02bed749dd56e287bccea
LomovaAA <anastlomova14@gmail.com>
2020-10-18 15:57:49 +0300

    Реализовано удаление заметок

--- lomova/notes/src/app/container/container.component.ts
+++ lomova/notes/src/app/container/container.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { ISection } from '../section/isection';
 
 @Component({
   selector: 'app-container',
@@ -7,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container.component.css']
 })
 export class ContainerComponent implements OnInit {
-  // _faTrashAlt = faTrashAlt;
+  sections: ISection[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.sections.push(
+      {
+        sectionTitle: "Секция 1", notes: [
+          {
+            noteTitle: "Заметка 1",
+            noteText: "Текст заметки 1 секции 1",
+            noteDate: "14.10.20",
+            noteId: 1
+          },
+          {
+            noteTitle: "Заметка 2",
+            noteText: "Текст заметки 2 секции 1",
+            noteDate: "14.10.20",
+            noteId: 2
+          }
+        ]
+      },
+      {
+        sectionTitle: "Секция 2", notes: [
+          {
+            noteTitle: "Заметка 1",
+            noteText: "Текст заметки 1 секции 2",
+            noteDate: "14.10.20",
+            noteId: 3
+          }
+        ]
+      });
   }
-
 }
---
+++ lomova/notes/src/app/section/isection.ts
@@ -0,0 +1,5 @@
+import {INote} from '../note/inote';
+export interface ISection {
+    sectionTitle: string;
+    notes: INote[];
+}
