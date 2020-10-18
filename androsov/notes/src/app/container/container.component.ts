c9e81a3ddfdb41dd33e2508932a5330d9369337f
mxzpytlk <mxzpytlk@gmail.com>
2020-10-18 15:02:29 +0300

    Section and notes create dynamically

--- androsov/notes/src/app/container/container.component.ts
+++ androsov/notes/src/app/container/container.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { INote } from '../section/note.interface';
+import { ISection } from './section.interface';
 
 @Component({
   selector: 'app-container',
@@ -9,7 +11,19 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 export class ContainerComponent implements OnInit {
   faPlus = faPlus;
 
-  constructor() { }
+  sections: ISection[] = [];
+  section1: ISection = {
+    header : 'Section1',
+    notes : [{
+      header : 'Note 1',
+      content : 'This is first note',
+      date : '18.10.2020 14:51'
+    }]
+  }
+
+  constructor() { 
+    this.sections.push(this.section1);
+  }
 
   ngOnInit(): void {
   }
--- androsov/notes/src/app/section/section.component.ts
+++ androsov/notes/src/app/section/section.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { faCogs } from '@fortawesome/free-solid-svg-icons';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit, Input } from '@angular/core';
+import { faCogs, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { INote } from './note.interface';
+import { ISection } from '../container/section.interface';
 
 @Component({
   selector: 'app-section',
   templateUrl: './section.component.html',
   styleUrls: ['./section.component.css']
 })
-export class SectionComponent implements OnInit {
+export class SectionComponent implements OnInit, ISection {
   faCogs = faCogs;
   faPlus = faPlus;
 
   constructor() { }
+  @Input() header: string;
+  @Input() notes: INote[];
 
   ngOnInit(): void {
   }
