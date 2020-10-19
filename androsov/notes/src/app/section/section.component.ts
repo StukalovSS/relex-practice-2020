7edf30509ae5ac1de6e37a9628e806052affab35
mxzpytlk <mxzpytlk@gmail.com>
2020-10-19 19:11:20 +0300

    User can add notes

--- androsov/notes/src/app/section/section.component.ts
+++ androsov/notes/src/app/section/section.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { faCogs, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { INote } from './note.interface';
 import { ISection } from '../container/section.interface';
+import { FormBuilder, FormControl, FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-section',
@@ -12,12 +13,21 @@ export class SectionComponent implements OnInit, ISection {
   faCogs = faCogs;
   faPlus = faPlus;
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.addNoteForm = fb.group({
+      noteHeader : new FormControl(''),
+      noteText : new FormControl('')
+    });
+  }
+
   @Input() header: string;
   @Input() notes: INote[];
 
   ngOnInit(): void {
   }
+
+  addNoteForm: FormGroup;
+  invisibleForm: boolean = true;
 
   deleteNote(id: number): void {
     let deletingNoteIndex = this.notes.findIndex( note => note.id === id);
@@ -26,5 +36,18 @@ export class SectionComponent implements OnInit, ISection {
     }
 
     this.notes.splice(deletingNoteIndex, 1);
+  }
+
+  changeFormVisibillity() {
+    this.invisibleForm = !this.invisibleForm;
+  }
+
+  addNote() {
+    this.notes.push({
+      header : this.addNoteForm.value.noteHeader,
+      content : this.addNoteForm.value.noteText,
+      date : '',
+      id : this.notes.length
+    });
   }
 }
