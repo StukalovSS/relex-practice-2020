174844c241e18d6471274ecf4e7c5c6b65eac157
Roman Baklanov <wackier.leaf@yandex.ru>
2020-10-23 12:10:24 +0300

    sync

---
+++ Baklanov/AngularApps/PushPin/src/app/services/data.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { INote } from '../note/note.interface';
+import { ISection } from '../section/section.interface';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private sections: ISection[] = [];
+  private sectionId: number = 0;
+  private noteId: number = 0;
+  private currentSection: number;
+  getData(): ISection[] {
+    return this.sections;
+  }
+  addSection(section: ISection) {
+    section.id = this.sectionId++;
+    this.sections.push(section);
+    this.currentSection = (this.sections.length - 1);
+  }
+  getLastSection(): ISection {
+    return this.sections[this.sections.length - 1];
+  }
+  getNote(): INote {
+    return this.sections[this.currentSection].notes[this.sections[this.currentSection].notes.length - 1];
+  }
+  addNoteBySectionId(id: number, note: INote) {
+    for (let i = 0; i < this.sections.length; i++) {
+      if (this.sections[i].id == id) {
+        this.currentSection = i;
+        note.id = this.noteId++;
+        this.sections[i].notes.push(note);
+      }
+    }
+  }
+  getSectionById(id: number): ISection {
+    for (let i = 0; i < this.sections.length; i++) {
+      if (this.sections[i].id == id) {
+        return this.sections[i];
+      }
+    }
+  }
+  deleteNote(sectionId: number, noteId: number): void {
+    for (let i = 0; i < this.sections.length; i++) {
+      if (this.sections[i].id == sectionId) {
+        for (let j = 0; j < this.sections[i].notes.length; j++) {
+          if (this.sections[i].notes[j].id == noteId) {
+            this.sections[i].notes.splice(j, 1);
+          }
+        }
+      }
+    }
+  }
+  deleteSectionById(sectionId: number): void {
+    for (let i = 0; i < this.sections.length; i++) {
+      if (this.sections[i].id == sectionId) {
+        this.sections.splice(i, 1);
+        this.currentSection = i - 1;
+      }
+    }
+  }
+  editSection(id: number, section: ISection) {
+    let index = this.sections.findIndex(section => section.id == id)
+    this.sections[index] = section;
+  }
+}
