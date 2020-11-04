e1aa2684bc3676990d79c1deff57f723ef4037f3
mxzpytlk <mxzpytlk@gmail.com>
2020-11-04 18:52:08 +0300

    Реализован роутинг

--- androsov/notes/src/app/sections/container/container.component.ts
+++ androsov/notes/src/app/sections/container/container.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { faPlus, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import { SectionsDataService } from './../sections-data.service';
 
@@ -19,13 +20,26 @@ export class ContainerComponent implements OnInit {
 
   sectionHeaderInput: FormGroup;
 
-  constructor( fb: FormBuilder, public data: SectionsDataService ) {
+  constructor( fb: FormBuilder, public data: SectionsDataService, private router: Router, private activatedRoute: ActivatedRoute) {
     this.sectionHeaderInput = fb.group({
       sectionHeader: new FormControl('', Validators.required )
     });
   }
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe( params => {
+      if (params.even) {
+        (document.getElementById('all-sections-even') as HTMLInputElement).checked = params.even === 'true';
+      }
+      if (params['not-even']) {
+        (document.getElementById('all-sections-not-even') as HTMLInputElement).checked = params['not-even'] === 'true';
+      }
+      if (params['sort-ascending']) {
+        Array.from( document.querySelectorAll('input') ).filter( input => {
+          return input.name === 'all-sections-sort-ascending';
+        })[params['sort-ascending'] === 'true' ? 0 : 1].checked = true;
+      }
+    });
   }
 
   addSection(): void {
@@ -38,5 +52,20 @@ export class ContainerComponent implements OnInit {
    */
   changeVisibillity(): void {
     this.invisible = !this.invisible;
+  }
+
+  /**
+   * Изменение параметров url в зависимости от установленных флагов.
+   */
+  changeAllSectionsViewFromat(): void {
+    this.router.navigate(['home'], {
+      queryParams: {
+        even: (document.getElementById('all-sections-even') as HTMLInputElement).checked,
+        'not-even': (document.getElementById('all-sections-not-even') as HTMLInputElement).checked,
+        'sort-ascending': Array.from( document.querySelectorAll('input') ).filter( input => {
+          return input.name === 'all-sections-sort-ascending';
+        })[0].checked
+      }
+    });
   }
 }
