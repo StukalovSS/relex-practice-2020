ac9b56766b129721a0ec830bf0eb55f5ff09d2ca
Roman Baklanov <wackier.leaf@yandex.ru>
2020-11-05 09:31:56 +0300

    sync

--- Baklanov/AngularApps/PushPin/src/app/app-routing.module.ts
+++ Baklanov/AngularApps/PushPin/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ModalNoteComponent } from './modules/modal/modal-note/modal-note.component';
 import { ModalSectionComponent } from './modules/modal/modal-section/modal-section.component';
+import {ContainerComponent} from './components/container/container.component'
 const routes: Routes = [
   { path: 'modal-note', component: ModalNoteComponent },
   { path: 'modal-section', component: ModalSectionComponent },
