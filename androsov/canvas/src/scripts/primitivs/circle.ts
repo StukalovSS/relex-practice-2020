397d857dd27cd36a6d125aa41e144e48634396d5
mxzpytlk <mxzpytlk@gmail.com>
2020-11-09 15:08:06 +0300

    Merge branch 'master' of https://github.com/StukalovSS/relex-practice-2020

--- androsov/canvas/src/scripts/primitivs/circle.ts
+++ androsov/canvas/src/scripts/primitivs/circle.ts
@@ -1,16 +1,9 @@
-
-
 export default class Circle {
-    public pos: any;
-    public vel: any;
-    
-    constructor(x: number, y: number, public r: number, protected s: any) {
-        this.pos = s.createVector(x, y);
-        this.vel = s.createVector(0, 0); 
-    }
+    constructor(public x: number, public y: number, public r: number) {}
 
-    show() {
-        this.s.fill(255);
-        this.s.ellipse(this.pos.x, this.pos.y, this.r * 2)
+    show(gr: CanvasRenderingContext2D) {
+        gr.beginPath();
+        gr.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+        gr.fill();
     } 
 }
