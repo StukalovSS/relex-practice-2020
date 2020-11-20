6e925dcdad9404668df2fae0e4dc09c07329578d
YarkinaMA <marinayarkina@icloud.com>
2020-11-20 13:00:35 +0300

    Исправлены ошибки версий

--- MYarkina/notes/src/app/modalwindow/modalwindownote/modalwindownote.component.ts
+++ MYarkina/notes/src/app/modalwindow/modalwindownote/modalwindownote.component.ts
@@ -1,5 +1,5 @@
 import { DatePipe } from '@angular/common';
-import { Component, OnInit, Output,EventEmitter, Input} from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { INote } from '../../container/note.interface';
 
@@ -9,30 +9,11 @@ import { INote } from '../../container/note.interface';
   styleUrls: ['./modalwindownote.component.scss']
 })
 /**
-<<<<<<< HEAD
- * Класс модальное окно для создания новой заметки
- */
-export class ModalwindownoteComponent implements OnInit{
-
-<<<<<<< HEAD
-  @Input() note:INote;
-
-  /**
-   * Форма для создания новой заметки.
-   */
-  myForm:FormGroup;
-  private createForm(name,text) {
-    var today = new DatePipe("en-US").transform(new Date(), "dd.MM.yyyy HH:mm");
-=======
-  @Output() output = new EventEmitter();
-  @Input() note: INote;
-=======
  * Класс модальное окно добавления и редактирования заметки.
  * Содержит форму для добавления или редактирования заметки
  */
 export class ModalwindownoteComponent implements OnInit{
 
->>>>>>> Добавлен выпадающий список с выбором языка приложения. Добавлены модификаторы доступа у методов класса. Поправлены отступы
   myForm: FormGroup;
   statusForm: string;
 
@@ -47,41 +28,20 @@ export class ModalwindownoteComponent implements OnInit{
    */
   private createForm(name, text): void {
     const today = new DatePipe('en-US').transform(new Date(), 'dd.MM.yyyy HH:mm');
->>>>>>> Добавлена возможность перетаскивание заметок внутри секции
     this.myForm = new FormGroup({
-      'name': new FormControl(name,Validators.required),
-      'text': new FormControl(text,Validators.required),
-      'date': new FormControl(today,Validators.required)
-    })
+      name: new FormControl(name, Validators.required),
+      text: new FormControl(text, Validators.required),
+      date: new FormControl(today, Validators.required)
+    });
   }
 
   ngOnInit(): void {
-<<<<<<< HEAD
-    if(this.note){
-      this.createForm(this.note.name,this.note.nodeTxt);
-    }
-    else{
-      this.createForm("","");
-=======
     if (this.note){
       this.createForm(this.note.name, this.note.nodeTxt);
       this.statusForm = 'change';
     }
     else{
       this.createForm('', '');
-<<<<<<< HEAD
-      this.statusForm = 'Добавить';
->>>>>>> Добавлена возможность перетаскивание заметок внутри секции
-    }
-  }
-
-  @Output() output = new EventEmitter();
-  addNewNote(myForm){
-    this.output.emit(myForm);
-  }
-
-  closeForm(){
-=======
       this.statusForm = 'add';
     }
   }
@@ -98,8 +58,7 @@ export class ModalwindownoteComponent implements OnInit{
    * Закрывает окно.
    */
   public closeForm(): void{
->>>>>>> Добавлен выпадающий список с выбором языка приложения. Добавлены модификаторы доступа у методов класса. Поправлены отступы
     this.output.emit(false);
   }
-  
+
 }
